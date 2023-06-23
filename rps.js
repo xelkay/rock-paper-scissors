ec57abba34014bf39b7d033902b0a2de80e63f22
xelkay <136422847+xelkay@users.noreply.github.com>
2023-06-23 13:10:01 +0530

    Added game function that makes the game 5 rounds and displays winner at the end

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+
+let playerScore = 0;
+let computerScore = 0;
+    
 function getComputerChoice() {
     let things = ['rock', 'paper', 'scissors'];
     let thing = things[Math.floor(Math.random()*things.length)];
@@ -5,29 +9,53 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+
+
     if (playerSelection===computerSelection) {
         return ("It's a draw!");
     }
     else if (playerSelection==="rock" && computerSelection== "paper") {
+        computerScore++;
         return ("You Lose! Paper beats rock");
     } 
     else if (playerSelection== "paper" && computerSelection==="rock"){
+        playerScore++;
         return ("You Win! Paper beats rock");
     }
     else if (playerSelection==="rock" && computerSelection==="scissors"){
+        playerScore++;
         return ("You Win! Rock beats scissors");
     }
     else if (playerSelection==="scissors" && computerSelection==="rock"){
+        computerScore++;
         return ("You Lose! Rock beats scissors");
     }
     else if (playerSelection== "paper" && computerSelection==="scissors"){
+        computerScore++;
         return ("You Lose! Scissors beat paper");
     }
     else if (playerSelection==="scissors" && computerSelection== "paper"){
+        playerScore++;
         return ("You Win! Scissors beat paper");
     }
+
 }
-    let playerSelection = prompt("Choose your weapon");
-    let computerSelection = getComputerChoice();
-    console.log(computerSelection)
-    console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose your weapon");
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    
+    if (playerScore > computerScore) {
+        console.log("You are the winner!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer wins");
+    } else {
+        console.log("It's a tie!");
+    }
+}
+
+game();
